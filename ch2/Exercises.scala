cb41466bd6ad66d04f4d15a108423e459d5b3eff
Enrique A. Tobis <enrique@tobis.com.ar>
2015-05-05 19:58:43 -0400

    Chapter 2 Exercises

---
+++ ch2/Exercises.scala
@@ -0,0 +1,54 @@
+object Exercises {
+    // 2.1
+    def fib(n: Int): Int = {
+	@annotation.tailrec
+	def fibAux(n: Int, a: Int, b: Int): Int = {
+	    if (n == 0) {
+		b
+	    } else {
+		fibAux(n - 1, a + b, a)
+	    }
+	}
+	fibAux(n, 1, 0)
+    }
+
+    // 2.2
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+	@annotation.tailrec
+	def isSortedAux(as: Array[A], ordered: (A, A) => Boolean,
+			n: Int): Boolean =
+	{
+	    if (n + 1 >= as.length)
+		true;
+	    else if (ordered(as(n), as(n+1)))
+		isSortedAux(as, ordered, n+1);
+	    else
+		false;
+	}
+	isSortedAux(as, ordered, 0);
+    }
+
+    // 2.3
+    def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+	a => (b => f(a, b))
+    }
+
+    // 2.4
+    def uncurry[A, B, C](f: A => (B => C)): (A, B) => C = {
+	(a, b) => f(a)(b)
+    }
+    
+    // 2.5
+    def compose[A, B, C](f: A => B, g: B => C): A => C = {
+	a => g(f(a))
+    }
+
+    def main(args: Array[String]): Unit = {
+	println(fib(Integer.parseInt(args(0))))
+	def lowerThan(a: Int, b: Int): Boolean = {
+	    return a <= b;
+	}
+	println(isSorted(Array(2, 1), lowerThan));
+
+    }
+}
